daac5cfa2f56511248f79a14882b058c7f9a258e
Flip <j4324@gmx.de>
2019-01-04 19:47:49 +0100

    add Model, change style

---
+++ src/main/java/com/github/ricommunity/calendar/Event.java
@@ -0,0 +1,73 @@
+package com.github.ricommunity.calendar;
+
+public class Event {
+	private String title;
+	private String date;
+	private String time;
+	private String address;
+	private String information;
+	private String link;
+
+	public Event() {
+		this("Title","date","time","adress","info","http://google.de");
+	}
+		public Event(String title, String date, String time, String address, String information, String link) {
+		this.title = title;
+		this.date = date;
+		this.time = time;
+		this.address = address;
+		this.information = information;
+		this.link = link;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getInformation() {
+		return information;
+	}
+
+	public void setInformation(String information) {
+		this.information = information;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+	
+	
+
+}
--- src/main/java/com/github/ricommunity/calendar/WebController.java
+++ src/main/java/com/github/ricommunity/calendar/WebController.java
@@ -1,5 +1,8 @@
 package com.github.ricommunity.calendar;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +13,9 @@ public class WebController {
 
     @GetMapping("/")
     public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+        Event event = new Event();
+        List<Event> events = Arrays.asList(event);
+        model.addAttribute("events", events);
         return "home";
     }
 
